fec38c8bc4bf7cbd5cb1412d8f252ac3caeecd58
Archana Balakrishna Lokhande <145834338+Archana-547@users.noreply.github.com>
2023-10-20 23:50:46 +0300

    Homework_07 completed

--- src/main/java/org/example/MathUtils.java
+++ src/main/java/org/example/MathUtils.java
@@ -1,10 +1,32 @@
 package org.example;
 
+import org.w3c.dom.ls.LSOutput;
+
+import java.sql.SQLOutput;
+
 public class MathUtils {
 
     public static int add(int a, int b) {
 
         return a + b;
+    }
+
+
+    public static double divide(double a, double b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Division by zero is not allowed");
+        }
+        return a / b;
+    }
+
+    // Method for multiplying floating point numbers
+    public static double multiply(double a, double b) {
+        return a * b;
+    }
+
+    // Method for squaring a number
+    public static double square(double num) {
+        return num * num;
     }
 
 
